187550c543d03f53aeb8aa525260a288f0d42241
padma_k <padma.karthikeyan@bhnetwork.com>
2020-06-28 17:46:50 -0700

    removed text

--- Book_list/services/twilio_service.py
+++ Book_list/services/twilio_service.py
@@ -6,14 +6,14 @@ class TwilioService:
 
     def __init__(self):
         # Find these values at https://twilio.com/user/account
-        account_sid = 'AC2db85943534ca50e7d2bac1c0ddeac72'
-        auth_token = '996c202e8f163f8f509d21a8da81d3f0'
+        account_sid = 'account_sid'
+        auth_token = 'auth_token'
         print(auth_token)
         self.client = Client(account_sid, auth_token)
 
     def send_message(self, message, recipientphone):
 
-        twilio_phone_number = '+12058518364'
+        twilio_phone_number = 'twilio_phone_number'
         print(recipientphone)
         self.client.messages.create(to=recipientphone,
                                     from_=twilio_phone_number,
